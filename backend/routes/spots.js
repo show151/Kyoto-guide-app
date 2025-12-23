2fcc129d71ed24d7d47c7a0c0664886f406a6e32
Sho Kawano <136795114+show151@users.noreply.github.com>
2025-12-23 11:44:36 +0900

    Revert "Built backend environment"

--- backend/routes/spots.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getSpots, getSpotById } = require('../controllers/spotsController');
-
-router.get('/', getSpots);
-
-router.get('/:id', getSpotById);
-
-module.exports = router;
